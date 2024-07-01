7d614c8265c9127e1e8a20f9847005433f7b586c
Aditi <aditideshpande2908@gmail.com>
2024-07-01 15:01:19 +0530

    fetch the countries' info from API using state & effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Header from './components/Header.jsx'
 import Card from './components/Card.jsx'
 
 function App() {
+	const [cardCollection, setCardCollection] = useState([])
+	console.log('component rendered')
+
+	useEffect(() => {
+		console.log('effect running...')
+		fetch('https://restcountries.com/v3.1/all')
+			.then((response) => response.json())
+			.then((data) => setCardCollection(data))
+	}, [])
+
+	console.log(cardCollection.length && cardCollection[76].name.common)
+
+	const cardElements = cardCollection.map((eachCard) => (
+		<Card
+			key={eachCard.ccn3}
+			name={eachCard.name.common}
+			population={eachCard.population}
+			region={eachCard.region}
+			capital={eachCard.capital}
+			flag={eachCard.flags.svg}
+		/>
+	))
+
 	return (
 		<>
 			<Header />
-			<Card
-				name='United States of America'
-				population='323,947,000'
-				region='Americas'
-				capital='Washington. D. C'
-				flag='https://flagcdn.com/us.svg'
-			/>
+			<main className='main'>{cardElements}</main>
 		</>
 	)
 }
