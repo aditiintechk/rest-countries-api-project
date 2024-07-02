c202158abd15499a77f7b65c17b5fde2023910fc
Aditi <aditideshpande2908@gmail.com>
2024-07-02 15:22:08 +0530

    remove imgs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,6 @@ export default function Header(props) {
 			<header className={props.lightMode ? 'header light' : 'header'}>
 				<h1 className='header--title'>Where in the world?</h1>
 				<div className='header--mode' onClick={props.toggleMode}>
-					<img
-						className='mode__img'
-						src={
-							props.lightMode
-								? '../assests/light-mode.svg'
-								: '../assests/dark-mode.svg'
-						}
-						alt='white color moon symbol to represent dark mode of the website'
-					/>
 					<h2 className='mode__text'>
 						{props.lightMode ? 'Light' : 'Dark'} Mode
 					</h2>
